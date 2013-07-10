c68cd669bbc26a72bcf9e8dcd61f2f549a0c2231
Jason R. Coombs <jaraco@jaraco.com>
2013-07-09 20:29:31 -0400

    Declare dependency on six
    --HG--
    extra : source : e2d079b555a787d96ef2f688b5e56e9e7dada322

--- setup.py
+++ setup.py
@@ -40,7 +40,10 @@ import sys
 try:
     from setuptools import setup
     kw = {
-        'install_requires': 'pycrypto >= 2.1, != 2.4',
+        'install_requires': [
+            'pycrypto >= 2.1, != 2.4',
+            'six',
+        ],
     }
 except ImportError:
     from distutils.core import setup
